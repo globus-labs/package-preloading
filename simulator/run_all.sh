80d86fcd85baa82012985415b15c444a10fcd9ec
gonthier <maxjp.gr@gmail.com>
2024-05-23 11:31:07 -0500

    script

---
+++ simulator/run_all.sh
@@ -0,0 +1,6 @@
+# bash run_all.sh
+
+bash run.sh ../all_top_10_binder_sorted.csv outputs/all_top_10_binder_sorted.csv 5 all_top_10
+bash run.sh ../cluster_top_10_binder_sorted.csv outputs/cluster_top_10_binder_sorted.csv 5 cluster_top_10
+bash run.sh ../vanilla_approach_binder_sorted.csv outputs/vanilla_approach_binder_sorted.csv 5 vanilla_approach
+bash run.sh ../on_the_fly_binder_sorted.csv outputs/on_the_fly_binder_sorted.csv 5 on_the_fly
